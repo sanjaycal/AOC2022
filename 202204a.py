41d7ffb23cff421042d4566e8c6af8c7fc0411f7
Sanjay <Sanjay.calgry@gmail.com>
2022-12-03 22:08:07 -0700

    Day 4 completed

---
+++ 202204a.py
@@ -0,0 +1,18 @@
+
+
+
+data = open("202204i.txt","r").read().split("\n")
+
+c = 0
+
+for i in data:
+    p1 = i.split(",")[0].split("-")
+    p2 = i.split(",")[1].split("-")
+    p1n = [int(p1[0]),int(p1[1])]
+    p2n = [int(p2[0]),int(p2[1])]
+    if p1n[0] <= p2n[0] and p1n[1] >= p2n[1]:
+        c+=1
+    elif p1n[0] >= p2n[0] and p1n[1] <= p2n[1]:
+        c+=1
+
+print(c)
